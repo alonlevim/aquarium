10d013695458c6457bdc528891938557addfcc2e
Alon <alonlevim@gmail.com>
2019-12-05 12:16:51 +0200

    Updates

---
+++ precache-manifest.21a49d655ca119189fcdfd64e0c2c600.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77b2c72f4048a19b2f98991f0342e501",
+    "url": "/aquarium/index.html"
+  },
+  {
+    "revision": "bb2973d6b618145a5455",
+    "url": "/aquarium/static/css/main.351e0206.chunk.css"
+  },
+  {
+    "revision": "8a9e8373d9d89dcce9ed",
+    "url": "/aquarium/static/js/2.d3a8a342.chunk.js"
+  },
+  {
+    "revision": "bb2973d6b618145a5455",
+    "url": "/aquarium/static/js/main.ac304df1.chunk.js"
+  },
+  {
+    "revision": "ee3c6bc92623e9320291",
+    "url": "/aquarium/static/js/runtime-main.62bd49fc.js"
+  },
+  {
+    "revision": "40d840ac15b97571808d3ec6de349cf2",
+    "url": "/aquarium/static/media/blue-shape.40d840ac.svg"
+  },
+  {
+    "revision": "15d7a192ff19a2d197c5f0b186eaea88",
+    "url": "/aquarium/static/media/blue-shape2.15d7a192.svg"
+  },
+  {
+    "revision": "691261466f75b80c1455af2ed105b55b",
+    "url": "/aquarium/static/media/bubble.69126146.svg"
+  },
+  {
+    "revision": "a6ec6b727126a29c552a835695ae0f05",
+    "url": "/aquarium/static/media/fish.a6ec6b72.svg"
+  },
+  {
+    "revision": "0baeed56ae23995047426c0fdc0721f9",
+    "url": "/aquarium/static/media/green-shape.0baeed56.svg"
+  },
+  {
+    "revision": "80a36b2c2fe5a55bcf80b91766c63199",
+    "url": "/aquarium/static/media/green-shape2.80a36b2c.svg"
+  },
+  {
+    "revision": "1bb027109345a90a9eab1e929d8669c2",
+    "url": "/aquarium/static/media/iconmonstr-github-1.1bb02710.svg"
+  },
+  {
+    "revision": "5010d4f3496a565f414611da003dc70c",
+    "url": "/aquarium/static/media/land.5010d4f3.svg"
+  },
+  {
+    "revision": "afc4ef8479fd140f340383a470a9cf11",
+    "url": "/aquarium/static/media/purple-shape.afc4ef84.svg"
+  },
+  {
+    "revision": "8d493c82ef7ed63a897d101b426473ef",
+    "url": "/aquarium/static/media/red-shape.8d493c82.svg"
+  },
+  {
+    "revision": "771f0d0be1f681ce9d4464e32b8c8624",
+    "url": "/aquarium/static/media/rock.771f0d0b.svg"
+  },
+  {
+    "revision": "0c047bfca851b340290785abae8b2d1f",
+    "url": "/aquarium/static/media/rock2.0c047bfc.svg"
+  },
+  {
+    "revision": "8fa88985281897ff55da2c2ee05b3bc6",
+    "url": "/aquarium/static/media/rocks.8fa88985.svg"
+  },
+  {
+    "revision": "38be889c94c82b8d21131e3042be42fe",
+    "url": "/aquarium/static/media/sea-kelp.38be889c.svg"
+  },
+  {
+    "revision": "7e075853cf29fd84716bdd0ec7aec804",
+    "url": "/aquarium/static/media/standing.7e075853.svg"
+  },
+  {
+    "revision": "aa55cd6db9485d35c6118590fe941fa0",
+    "url": "/aquarium/static/media/standing2.aa55cd6d.svg"
+  },
+  {
+    "revision": "5ec30dde7034b555b07458fd29ca2bcc",
+    "url": "/aquarium/static/media/sunlight.5ec30dde.svg"
+  }
+]);
